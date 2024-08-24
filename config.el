da742107894f53b53c311e9b0b506f09e7b80045
pascualmg <pascual.munoz@pccomponentes.com>
2024-08-24 15:49:03 +0200

    ahora sis desde el mac

---
+++ config.el
@@ -0,0 +1,149 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+ (setq user-full-name "Pascual M.G."
+       user-mail-address "pascual.munoz.galian@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+;; (setq doom-theme 'doom-one)
+
+(setq doom-font (font-spec :family "Hack Nerd Font Mono JBM Ligatured CCG" :size 14)
+      doom-variable-pitch-font (font-spec :family "Hack Nerd Font JBM Ligatured CCG" :size 15)
+      doom-big-font (font-spec :family "Hack Nerd Font Mono JBM Ligatured CCG" :size 24)
+      doom-symbol-font (font-spec :family "Hack Nerd Font Mono JBM Ligatured CCG")
+      doom-serif-font (font-spec :family "Hack Nerd Font Mono JBM Ligatured CCG" :weight 'light))
+
+;; Habilitar ligaduras
+(setq +ligatures-in-modes t)
+;; Mensaje para verificar la configuración actual de la fuente
+(message "Valor actual de doom-font: %s" doom-font)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Dropbox/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+;; Configuración de PHP con Nix
+(after! php
+  (setq php-mode-coding-style 'psr2) ; Estilo de codificación PSR-2
+
+  ;; Ruta al ejecutable de PHP proporcionado por Nix
+  (setq php-mode-program "/nix/store/07vcszwmkfxhdjnsi5psxzr47zg9xkjj-php-with-extensions-8.2.13/bin/php")
+  )
+
+  (setq dap-php-debug-program '("/nix/store/07vcszwmkfxhdjnsi5psxzr47zg9xkjj-php-with-extensions-8.2.13/bin/php" "-dxdebug.remote_enable=1" "-dxdebug.remote_mode=req" "-dxdebug.remote_port=9000" "-dxdebug.remote_host=127.0.0.1" "-dxdebug.remote_connect_back=0")
+  )
+
+(setq telega-server-libs-prefix "/usr/local/lib/tdlib")
+
+(use-package! doom-themes
+  :config
+  (setq doom-themes-org-font "-apple-Pacifico-Regular-normal-normal-*-14-*-*-*-m-0-iso10646-1"))
+
+
+;;para que se exporten en los htmls los results en los org
+(setq org-export-allow-bind-keywords t)
+
+;; Añade esto a tu archivo ~/.doom.d/config.el
+
+;; Habilitar transparencia
+(set-frame-parameter (selected-frame) 'alpha '(85 . 85))
+(add-to-list 'default-frame-alist '(alpha . (85 . 85)))
+
+;; Función para alternar la transparencia
+(defun toggle-transparency ()
+  (interactive)
+  (let ((alpha (frame-parameter nil 'alpha)))
+    (set-frame-parameter
+     nil 'alpha
+     (if (eql (cond ((numberp alpha) alpha)
+                    ((numberp (cdr alpha)) (cdr alpha))
+                    ;; También manejar casos no numéricos
+                    ((listp alpha) (cadr alpha)))
+              100)
+         '(85 . 85) '(100 . 100)))))
+
+;; Atajo de teclado para alternar la transparencia
+(global-set-key (kbd "C-c t") 'toggle-transparency)
+
+;; Si estás usando el tema doom-one, puedes intentar esto para un fondo más transparente
+(use-package! doom-themes
+  :config
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t)
+  (load-theme 'doom-one t)
+  (doom-themes-visual-bell-config)
+  (doom-themes-org-config)
+  ;; Ajusta el fondo para que sea completamente transparente
+  (set-face-background 'default "none"))
+
+;; Si lo anterior no funciona, puedes intentar esto como alternativa
+(unless (display-graphic-p)
+  (set-face-background 'default "none"))
+
+;; Personalización de colores para git-gutter
+(after! haskell-mode
+  (setq haskell-interactive-popup-errors nil))
